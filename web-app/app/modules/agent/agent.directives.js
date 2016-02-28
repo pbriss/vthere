6eaed460605f0ebcaf9723938e387f88fd46a36f
pbriss <pbriss@gmail.com>
2016-02-28 12:53:09 -0800

    Logo added and polish

--- web-app/app/modules/agent/agent.directives.js
+++ web-app/app/modules/agent/agent.directives.js
@@ -12,32 +12,99 @@
             restrict: 'A',
             link: function(scope, el) {
                 $timeout(function() {
-                    el.click(function() {
-                        scope.counters[0].update(scope.counters[0].endVal + 2);
-                        scope.counters[1].update(scope.counters[1].endVal + 6);
-                        scope.counters[2].update(scope.counters[2].endVal + 50);
+                    scope.counters[0].update(scope.counters[0].endVal + 2);
+                    scope.counters[1].update(scope.counters[1].endVal + 6);
+                    scope.counters[2].update(scope.counters[2].endVal + 50);
 
-                        $('#dollar').addClass('animated rubberBand');
+                    $('#dollar').addClass('animated rubberBand');
 
-                        $timeout(function() {
-                            $('#dollar').removeClass('animated rubberBand');
-                        }, 1000);
+                    $timeout(function() {
+                        $('#dollar').removeClass('animated rubberBand');
+                    }, 1000);
+                }, 3000);
 
-                        $.Notification.notify('custom', 'right middle', '@alexb', 'This is sooo awesome!');
-                        var $notify = $('.notifyjs-corner');
-                        $notify.insertAfter('#wrapper');
+                $timeout(function() {
+                    scope.counters[0].update(scope.counters[0].endVal + 2);
+                    scope.counters[1].update(scope.counters[1].endVal + 6);
+                    scope.counters[2].update(scope.counters[2].endVal + 50);
 
-                        $notify.css({position: 'absolute', left: $($('circle')[0]).offset().left, top: $($('circle')[0]).offset().top - 30});
-                        $notify.find('.notifyjs-wrapper').css({display: 'inline-block', width: '100px'});
+                    $('#dollar').addClass('animated rubberBand');
 
-                        $timeout(function() {
-                            $notify.addClass('animated zoomOutLeft');
-                            $timeout(function() {
-                                $notify.removeClass('animated zoomOutLeft');
-                            }, 2000);
-                        }, 2000);
-                    });
-                });
+                    $timeout(function() {
+                        $('#dollar').removeClass('animated rubberBand');
+                    }, 1000);
+                }, 2000);
+
+                $timeout(function() {
+                    scope.counters[0].update(scope.counters[0].endVal + 2);
+                    scope.counters[1].update(scope.counters[1].endVal + 6);
+                    scope.counters[2].update(scope.counters[2].endVal + 50);
+
+                    $('#dollar').addClass('animated rubberBand');
+
+                    $timeout(function() {
+                        $('#dollar').removeClass('animated rubberBand');
+                    }, 1000);
+                }, 2000);
+
+                $timeout(function() {
+                    scope.counters[0].update(scope.counters[0].endVal + 2);
+                    scope.counters[1].update(scope.counters[1].endVal + 6);
+                    scope.counters[2].update(scope.counters[2].endVal + 50);
+
+                    $('#dollar').addClass('animated rubberBand');
+
+                    $timeout(function() {
+                        $('#dollar').removeClass('animated rubberBand');
+                    }, 1000);
+                }, 2000);
+
+                $timeout(function() {
+                    scope.counters[0].update(scope.counters[0].endVal + 2);
+                    scope.counters[1].update(scope.counters[1].endVal + 6);
+                    scope.counters[2].update(scope.counters[2].endVal + 50);
+
+                    $('#dollar').addClass('animated rubberBand');
+
+                    $timeout(function() {
+                        $('#dollar').removeClass('animated rubberBand');
+                    }, 1000);
+                }, 2000);
+
+                $timeout(function() {
+                    scope.counters[0].update(scope.counters[0].endVal + 2);
+                    scope.counters[1].update(scope.counters[1].endVal + 6);
+                    scope.counters[2].update(scope.counters[2].endVal + 50);
+
+                    $('#dollar').addClass('animated rubberBand');
+
+                    $timeout(function() {
+                        $('#dollar').removeClass('animated rubberBand');
+                    }, 1000);
+                }, 2000);
+
+
+
+
+
+
+                //
+                //el.click(function() {
+                //
+                //    //$.Notification.notify('custom', 'right middle', '@alexb', 'This is sooo awesome!');
+                //    //var $notify = $('.notifyjs-corner');
+                //    //$notify.insertAfter('#wrapper');
+                ////
+                //    //$notify.css({position: 'absolute', left: $($('circle')[0]).offset().left, top: $($('circle')[0]).offset().top - 30});
+                //    //$notify.find('.notifyjs-wrapper').css({display: 'inline-block', width: '100px'});
+                ////
+                //    //$timeout(function() {
+                //    //    $notify.addClass('animated zoomOutLeft');
+                //    //    $timeout(function() {
+                //    //        $notify.removeClass('animated zoomOutLeft');
+                //    //    }, 2000);
+                //    //}, 2000);
+                //});
             }
         }
     }
