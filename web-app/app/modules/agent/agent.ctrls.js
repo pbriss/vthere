02e6037464d5ba6ff20947865cc5e0e5b80878f0
Alex Bekker <alex.bekker@udemy.com>
2016-03-04 11:50:37 -0800

    Agent edits

--- web-app/app/modules/agent/agent.ctrls.js
+++ web-app/app/modules/agent/agent.ctrls.js
@@ -8,14 +8,25 @@
     AgentController.$inject = ['$scope', '$timeout'];
     function AgentController($scope, $timeout) {
 
-        $scope.registered = 1523;
-        $scope.watching = 628;
-        $scope.revenue = 67899;
+        $scope.registered = Math.round(Math.random() * 5230 + 30);
+        $scope.watching = Math.round(Math.random() * $scope.registered);
+        $scope.revenue = Math.round(Math.random() * 75456 + 30);
         $scope.counters = [];
 
         $timeout(function() {
             $.VectorMap.init();
             $.DashboardCRM.init();
+
+            Morris.Donut({
+                element: 'morris-donut-chart',
+                data: [
+                    {label: "First Time", value: 45},
+                    {label: "2nd Time", value: 24},
+                    {label: "3rd Time", value: 16},
+                    {label: "4th Time", value: 10},
+                    {label: "5th+ Time", value: 5}
+                ]
+            });
         });
 
     }
